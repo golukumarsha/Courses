ae15a8a724e613954c0bbe25d15ab8a5b11b2588
golukumarsha <kumargolu83353@gmail.com>
2026-06-14 17:35:59 +0530

    Fix DATABASE_URL loading

--- database/connection.py
+++ database/connection.py
@@ -7,6 +7,9 @@ from database.db_model import Base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set!")
+
 engine = create_engine(DATABASE_URL)
 
 # ─── Tables auto-create ───────────────────────────────
